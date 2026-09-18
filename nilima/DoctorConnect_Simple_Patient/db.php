cbb95d243bbe79fe4c4f0005995d5a96a2c50fcd
Tasnim Khatun <tasnimkhatun1122@gmail.com>
2026-09-18 20:06:34 +0600

    Deleted Patient directory
    As the member is not attending

--- nilima/DoctorConnect_Simple_Patient/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "doctorconnect");
-
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
-
-$conn->set_charset("utf8mb4");
-?>
